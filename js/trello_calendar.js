d7f2e2977c1755a2de0c9798f1ef35739aeabfda
François de Metz <francois@2metz.fr>
2012-01-11 00:20:20 +0100

    Show all cards but mine are red.
    Signed-off-by: François de Metz <francois@stormz.me>

--- js/trello_calendar.js
+++ js/trello_calendar.js
@@ -4,21 +4,33 @@ $(document).ready(function() {
         success: onAuthorize
     });
 
+    var calendar = $('#calendar').fullCalendar({});
+
+    function listBoards(me) {
+        return function(boards) {
+            _(boards).each(function(board) {
+                Trello.get('/boards/'+ board.id +'/cards/all', {badges: true}).done(function(cards) {
+                    _(cards).each(function(card) {
+                        if (!card.badges.due) return;
+                        calendar.fullCalendar('renderEvent', {
+                            id: card.id,
+                            title: card.name,
+                            start: card.badges.due,
+                            color: _(card.idMembers).include(me.id) ? 'red' : 'green'
+                        }, true);
+                    });
+                });
+            });
+        }
+    }
+
+    function showMe(me) {
+        Trello.get('/members/my/boards', {filter: 'open'}).done(listBoards(me));
+    }
+
     function onAuthorize() {
         if (!Trello.authorized()) return Trello.authorize();
 
-        Trello.get('/members/my/cards', {badges: true}).done(function(cards) {
-
-            var calendar = $('#calendar').fullCalendar({});
-            _(cards).each(function(card) {
-                if (!card.badges.due) return;
-                calendar.fullCalendar('renderEvent', {
-                    id: card.id,
-                    title: card.name,
-                    start: card.badges.due,
-                    color: 'red'
-                }, true);
-            });
-        });
+        Trello.members.get('me').done(showMe);
     }
 });
