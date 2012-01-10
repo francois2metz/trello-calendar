3a45b3be6c12b9c62992dc6abd847dab1f9f38ad
François de Metz <francois@2metz.fr>
2012-01-10 23:49:15 +0100

    Only one request to get cards.
    Signed-off-by: François de Metz <francois@stormz.me>

--- js/trello_calendar.js
+++ js/trello_calendar.js
@@ -7,20 +7,17 @@ $(document).ready(function() {
     function onAuthorize() {
         if (!Trello.authorized()) return Trello.authorize();
 
-        Trello.get('/members/my/cards/all').done(function(cards) {
+        Trello.get('/members/my/cards', {badges: true}).done(function(cards) {
 
             var calendar = $('#calendar').fullCalendar({});
-
             _(cards).each(function(card) {
-                Trello.cards.get(card.id).done(function(card) {
-                    if (!card.badges.due) return;
-                    calendar.fullCalendar('renderEvent', {
-                        id: card.id,
-                        title: card.name,
-                        start: card.badges.due,
-                        color: 'red'
-                    }, true);
-                });
+                if (!card.badges.due) return;
+                calendar.fullCalendar('renderEvent', {
+                    id: card.id,
+                    title: card.name,
+                    start: card.badges.due,
+                    color: 'red'
+                }, true);
             });
         });
     }
